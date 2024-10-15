14cc2ac68bc03add970e9a4aa63affee9f1ef883
StrongBiceps <140565804+StrongBiceps@users.noreply.github.com>
2024-10-15 14:26:13 +0900

    Updated 2024.10.15

--- Algorithm/sorting/bubble_sort.cpp
+++ Algorithm/sorting/bubble_sort.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <vector>
 
-void bubbleSort(std::vector<int>& arr) 
+void bubbleSort(std::vector<int>& arr)
 {
     int n = arr.size();
     bool swapped;
 
     // n-1 회 반복
-    for (int i = 0; i < n - 1; ++i) 
+    for (int i = 0; i < n - 1; ++i)
     {
         swapped = false;
 
         // 인접한 요소 비교 및 교환
         for (int j = 0; j < n - i - 1; ++j)
         {
-            if (arr[j] > arr[j + 1]) 
+            if (arr[j] > arr[j + 1])
             {
                 std::swap(arr[j], arr[j + 1]);
                 swapped = true; // 교환이 발생했음을 기록
@@ -22,7 +22,7 @@ void bubbleSort(std::vector<int>& arr)
         }
 
         // 더 이상 교환이 없으면 정렬 완료
-        if (!swapped) 
+        if (!swapped)
         {
             break;
         }
@@ -37,7 +37,7 @@ void printArray(const std::vector<int>& arr)
     std::cout << std::endl;
 }
 
-int main() 
+int main()
 {
     std::vector<int> arr = { 64, 34, 25, 12, 22, 11, 90 };
 
