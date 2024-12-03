ee2982739b825b30e23d8ed287d655cae396c8dc
StrongBiceps <140565804+StrongBiceps@users.noreply.github.com>
2024-12-03 15:15:10 +0900

    Updated 2024.12.03

--- Algorithm/sorting/insertion_sort.cpp
+++ Algorithm/sorting/insertion_sort.cpp
@@ -12,3 +12,22 @@ end function
 
 // 삽입 정렬은 시퀀스가 이미 정렬되어 있을 때, 즉 최선의 경우에 O(n)의 시간 복잡도를 보이고
 // 최악의 경우에는 O(n^2)이다.
+
+void insertionSort(std::vector<int>& arr)
+{
+    int n = arr.size();
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // 현재 키 값보다 큰 요소들을 한 칸씩 오른쪽으로 이동
+        while (j >= 0 && arr[j] > key) 
+	{
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        // 키 값을 적절한 위치에 삽입
+        arr[j + 1] = key;
+    }
+}
