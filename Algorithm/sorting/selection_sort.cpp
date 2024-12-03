8b6fdbb5589e9ebf23ae0926b24b4c7b7a3c81a6
StrongBiceps <140565804+StrongBiceps@users.noreply.github.com>
2024-12-03 15:17:28 +0900

    Updated 2024.12.03

--- Algorithm/sorting/selection_sort.cpp
+++ Algorithm/sorting/selection_sort.cpp
@@ -14,3 +14,22 @@ function selectionSort(array A)
 end function
 
 // 선택 정렬은 최선, 최악의 경우 모두 O(n^2)의 시간 복잡도를 보인다.
+
+void selectionSort(std::vector<int>& arr) 
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) 
+    {
+        // 현재 위치에서 가장 작은 요소의 인덱스를 찾음
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) 
+	{
+            if (arr[j] < arr[minIndex]) 
+	    {
+                minIndex = j;
+            }
+        }
+        // 현재 위치의 요소와 가장 작은 요소를 교환
+        std::swap(arr[i], arr[minIndex]);
+    }
+}
